9ed92a58f19326eb4750019d1f07461205deb31c
bela <1981253197@qq.com>
2020-10-31 15:26:19 +0800

    完成标签修改、删除功能

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -11,7 +11,24 @@ const defaultTags = [
 const useTags = () => {
   const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
   const findTag = (id: number) => tags.filter((tag) => tag.id === id)[0];
-  return { tags, setTags, findTag }; //必须使用对象的形式导出，否则ts会报错
+  const findTagIndex = (id: number) => {
+    let result = -1;
+    for (let i = 0; i < tags.length; i++) {
+      if (tags[i].id === id) {
+        result = i;
+        break;
+      }
+    }
+    return result;
+  };
+  const updateTag = (id: number, {name}: { name: string }) => {
+      setTags(tags.map(tag=>tag.id===id?{id,name}:tag))
+  };
+
+  const deleteTag = (id: number) => {
+     setTags(tags.filter(tag=>tag.id!==id))
+  };
+  return { tags, setTags, findTag, findTagIndex, updateTag, deleteTag }; //必须使用对象的形式导出，否则ts会报错
 };
 
 export { useTags };
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -4,30 +4,62 @@ import { useTags } from "useTags";
 import Layout from "components/Layout";
 import Icon from "components/Icon";
 import { Button } from "components/Button";
-
+import { Input } from "components/Input";
+import styled from "styled-components";
+import { Center } from "components/Center";
+import { Space } from "components/Space";
+const Topbar = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 20px;
+  padding: 14px;
+  background: white;
+`;
+const InputWrapper = styled.div`
+  background: white;
+  padding: 0 16px;
+  margin-top: 8px;
+`;
 type Params = {
   id: string;
 };
 const Tag = () => {
-  const { id } = useParams<Params>(); //获取url中的hash参数
-  const { findTag } = useTags();
-  const tag = findTag(parseInt(id));
+  const { id: idString } = useParams<Params>(); //获取url中的hash参数
+  const { findTag, updateTag, deleteTag } = useTags();
+  const tag = findTag(parseInt(idString));
+  const tagContent = (tag: { id: number; name: string }) => {
+    return (
+      <div>
+        <InputWrapper>
+          <Input label="标签名" value={tag.name} onChange={(e) => updateTag(tag.id, { name: e.target.value })} />
+        </InputWrapper>
+        <div>
+          <Center>
+            <Space />
+            <Space />
+            <Button onClick={() => deleteTag(tag.id)}>删除标签</Button>
+          </Center>
+        </div>
+      </div>
+    );
+  };
   return (
     <Layout>
-      <header>
+      <Topbar>
         <Icon name="left" />
         <span>编辑标签</span>
         <Icon />
-      </header>
-      <div>
-        <label>
-          <span>标签名</span>
-          <input type="text" placeholder="标签名" />
-        </label>
-        <div>
-          <Button>删除标签</Button>
-        </div>
-      </div>
+      </Topbar>
+      {tag ? (
+        tagContent(tag)
+      ) : (
+        <Center>
+          <Space />
+          <Space />
+          <div>标签不存在</div>
+        </Center>
+      )}
     </Layout>
   );
 };
